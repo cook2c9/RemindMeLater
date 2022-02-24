1291c462faf2a384e6af7b211e468798d325e5c6
Falconsc <75039653+Falcons1231@users.noreply.github.com>
2022-02-24 17:01:58 -0500

    Added myself and other reminder button and list button

--- app/src/main/java/com/example/remindmelater/MainActivity.kt
+++ app/src/main/java/com/example/remindmelater/MainActivity.kt
@@ -1,28 +1,23 @@
 package com.example.remindmelater
 
 import android.os.Bundle
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.remindmelater.databinding.ActivityMapsBinding
+import androidx.compose.ui.unit.dp
 import com.example.remindmelater.ui.theme.RemindMeLaterTheme
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,45 +25,54 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
-                    Map()
                 }
             }
         }
     }
+}
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-
-    @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        RemindMeLaterTheme {
-            Greeting("Android")
+@Composable
+fun Greeting(name: String) {
+    val context = LocalContext.current
+    Column {
+        Text(text = "Hello, Set a Reminder for...")
+        Row (
+            horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+            Button(onClick = { Toast.makeText(context, "You clicked the button", Toast.LENGTH_LONG).show() },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .width(175.dp)
+                        .height(50.dp)
+                ){
+                Text(text = "Myself")
+            }
+            Button(onClick = { Toast.makeText(context, "You clicked the button", Toast.LENGTH_LONG).show() },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .width(174.dp)
+                        .height(50.dp)
+                ){
+                Text(text = "Others")
+            }
         }
+        Button(onClick = { Toast.makeText(context, "You clicked the button", Toast.LENGTH_LONG).show() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .height(35.dp)
+            ){
+            Text(text = "View Reminder List")
+        }
+
     }
 
-    @Composable
-    fun Map() {
-        Box()
-        {
-            val binding = ActivityMapsBinding.inflate(layoutInflater)
-            addContentView(binding.root, ViewGroup.LayoutParams(-1,-1))
+}
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-            mapFragment.getMapAsync(this@MainActivity)
-        }
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RemindMeLaterTheme {
+        Greeting("Android")
     }
 }

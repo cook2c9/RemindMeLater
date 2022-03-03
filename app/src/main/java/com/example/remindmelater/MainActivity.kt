3c30e9cb1d1a50ca0a20fd69710e1cdfedee6e7e
Capy <cgrothaus@outlook.com>
2022-03-03 11:53:54 -0500

    Added Map Fragment

--- app/src/main/java/com/example/remindmelater/MainActivity.kt
+++ app/src/main/java/com/example/remindmelater/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.remindmelater
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.view.ViewGroup
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.remindmelater.databinding.ActivityMapsBinding
 import com.example.remindmelater.ui.theme.RemindMeLaterTheme
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,21 +30,45 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
+                    Map()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RemindMeLaterTheme {
-        Greeting("Android")
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        RemindMeLaterTheme {
+            Greeting("Android")
+        }
+    }
+
+    @Composable
+    fun Map() {
+        Box()
+        {
+            val binding = ActivityMapsBinding.inflate(layoutInflater)
+            addContentView(binding.root, ViewGroup.LayoutParams(-1,-1))
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this@MainActivity)
+        }
     }
 }

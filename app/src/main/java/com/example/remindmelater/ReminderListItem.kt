54219f6e2e04a5a8277b6aa76a5e3adb9ff6d34d
Falconsc <75039653+Falcons1231@users.noreply.github.com>
2022-03-22 18:34:16 -0400

    Updated ReminderListItem

---
+++ app/src/main/java/com/example/remindmelater/ReminderListItem.kt
@@ -0,0 +1,56 @@
+package com.example.remindmelater
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.remindmelater.ui.theme.RemindMeLaterTheme
+
+@Composable
+fun ReminderListItem() {
+
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Reminder:")
+            Text(text = "For:")
+            Text(text = "Location:")
+            Text(text = "Range:")
+
+            Column() {
+                Row(
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    IconButton(onClick = {/* Do Something*/ }) {
+                        Icon(Icons.Filled.Edit, null)
+                    }
+                    IconButton(onClick = {/* Do Something*/ }) {
+                        Icon(Icons.Filled.Delete, null)
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun ListItemPreview() {
+    RemindMeLaterTheme {
+        ReminderListItem()
+    }
+}

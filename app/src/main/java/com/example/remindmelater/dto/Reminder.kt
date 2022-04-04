fa78dafe6f0a87e63a8e6255915fd691edc8b7c4
pokhrebi <pokhrebi@dev.azure.com>
2022-04-03 20:27:53 -0400

    Formatting - line longer than a page long

--- app/src/main/java/com/example/remindmelater/dto/Reminder.kt
+++ app/src/main/java/com/example/remindmelater/dto/Reminder.kt
@@ -1,5 +1,10 @@
 package com.example.remindmelater.dto
 
-data class Reminder(var title : String = "", var body: String = "", var latitude : Double = 0.0, var longitude : Double = 0.0, var radius : Int = 0, var userEmail : String = "") {
-
+data class Reminder(
+    var title : String = "",
+    var body: String = "",
+    var latitude : Double = 0.0,
+    var longitude : Double = 0.0,
+    var radius : Int = 0,
+    var userEmail : String = "") {
 }

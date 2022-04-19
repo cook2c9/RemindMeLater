09d6065ce90804899418e71e66c4e65219aeb1ce
Connor Cook <cook2c9@mail.uc.edu>
2022-04-18 22:50:31 -0400

    Unnecessary file cleanup

--- app/src/main/java/com/example/remindmelater/MainViewModel.kt
+++ app/src/main/java/com/example/remindmelater/MainViewModel.kt
@@ -1,57 +1,25 @@
 package com.example.remindmelater
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.remindmelater.ReminderRecyclerView.ReminderAdapter
 import com.example.remindmelater.dto.Reminder
-import com.example.remindmelater.service.IReminderService
-import com.example.remindmelater.service.ReminderService
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.coroutines.CompletableDeferred
 
-class MainViewModel(var reminderService : IReminderService = ReminderService()) : ViewModel() {
+class MainViewModel() : ViewModel() {
 
-    internal val NEW_REMINDER = "New Reminder"
     private lateinit var firestore : FirebaseFirestore
     private lateinit var auth : FirebaseAuth
     var reminders : MutableLiveData<List<Reminder>> = MutableLiveData<List<Reminder>>()
-    var selectedReminder by mutableStateOf(Reminder())
-    private lateinit var reminderAdapter: ReminderAdapter
 
     init{
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
         auth = FirebaseAuth.getInstance()
-    }
-
-    private fun listenToReminders() {
-        firestore.collection("reminders").addSnapshotListener {
-                snapshot, e ->
-            // Handle error
-            if(e != null){
-                Log.w("Listen failed", e)
-                return@addSnapshotListener
-            }
-            // Find the data
-            snapshot?.let{
-                val allReminders = ArrayList<Reminder>()
-                val documents = snapshot.documents
-                documents.forEach{
-                    val reminder = it.toObject(Reminder::class.java)
-                    reminder?.let {
-                        allReminders.add(it)
-                    }
-                }
-                reminders.value = allReminders
-            }
-        }
     }
 
     fun fetchReminders(reminders: SnapshotStateList<Reminder>){
--- app/src/main/java/com/example/remindmelater/service/IReminderService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.remindmelater.service
-
-import com.example.remindmelater.dto.Reminder
-
-interface IReminderService {
-    suspend fun fetchReminders() : List<Reminder>?
-}
